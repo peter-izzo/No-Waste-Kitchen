a50237d931f8ece9e19ca34cfc701dc9266b4a0e
Peter Izzo <peter.izzo528@gmail.com>
2020-12-09 00:55:34 -0500

    test layout on html and wokring ajax js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var edemamID = 'ecb6e209';
 var mealDBKey = '9973533';
 
 //
-var ingredients = "eggs"
+var ingredients = "";
 var meal = "Frosted%20Flakes"
 
 //API Keys and URLs for APIs
@@ -11,14 +11,23 @@ var mealDbURL = `https://www.themealdb.com/api/json/v2/${mealDBKey}/filter.php?i
 var edemamURL = `https://api.edamam.com/api/nutrition-data?app_id=${edemamID}&app_key=${edemamKey}&ingr=${meal}`;
 
 
+$(".submit").click(function() {
+    ingredients = $(".ingredients").val().replace(/\s/g,'');
+    $.ajax({
+        url: mealDbURL,
+        method: "GET"
+      }).then(function (resp) {
+          $(".recipe-choice").html("<p>" + resp.meals[1].strMeal);
+          console.log(resp.meals[1].strMeal + "</p>");
+          let img = $("<img>").attr("src", resp.meals[1].strMealThumb);
+          $(".recipe-choice").append(img);
+          console.log(resp);
+      });
+})
 
 
-$.ajax({
-    url: edemamURL,
-    method: "GET"
-  }).then(function (resp) {
-      console.log(resp);
-  });
+
+
 
 
 
