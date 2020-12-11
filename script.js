151748d6fe8a8d830d6da6f3a067f6a80cdea79a
Mike Henson <72236789+MikeH138@user.noreply.github.com>
2020-12-10 21:37:18 -0500

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// DOM selectors
+var item1 = document.querySelector('item1');
+var item2 = document.querySelector('item2');
+var item3 = document.querySelector('item3');
+var item4 = document.querySelector('item4');
+var searchResults = document.querySelector('recipeResults');
+
+// Event listener
+$(item1).submit(function(e) {
+  e.preventDefault;
+  ingredients = e.target.querySelector("item-1").value;
+});
+
+
+// API and Application Keys
 var edemamKey = '2da4dada354bb264573b3cc442d33d59';
 var edemamID = 'ecb6e209';
 var mealDBKey = '9973533';
@@ -11,7 +26,10 @@ var mealDbURL = `https://www.themealdb.com/api/json/v2/${mealDBKey}/filter.php?i
 var edemamURL = `https://api.edamam.com/api/nutrition-data?app_id=${edemamID}&app_key=${edemamKey}&ingr=${meal}`;
 
 
-$(".submit").click(function() {
+
+
+
+$(".button-primary").click(function() {
     ingredients = $(".ingredients").val().replace(/\s/g,'');
     $.ajax({
         url: mealDbURL,
