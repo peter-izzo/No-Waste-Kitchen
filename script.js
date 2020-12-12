12b8d3d1fa27d2c3da4709901bbc30f2bc7d4abb
Peter Izzo <peter.izzo528@gmail.com>
2020-12-12 13:55:31 -0500

    API calls actually work as intended. Need to make it make several so we get more than one meal

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 var edemamKey = '2da4dada354bb264573b3cc442d33d59';
 var edemamID = 'ecb6e209';
-var mealDBKey = '9973533';
+var mealDBPrefix = 'https://www.themealdb.com/api/json/v2/9973533';
 
 //
-var ingredients = "eggs";
+var ingredients = $(".ingredients").val();
 var meal;
 
 
-//API Keys and URLs for APIs
-var mealDbURL = `https://www.themealdb.com/api/json/v2/${mealDBKey}/filter.php?i=${ingredients}`;
+// ***API Keys and URLs for APIs ***
+
+// Used for nutritian facts
 var edemamURL = `https://api.edamam.com/api/nutrition-data?app_id=${edemamID}&app_key=${edemamKey}&ingr=${meal}`;
+
+// Gives more details about the meal than the above API
 var test = `https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`;
 
 //this function calls the api for a single meal showing all the ingredients and how to make it
@@ -18,38 +21,71 @@ function testAPI2(){
         type: "GET",
         url: test,
     }).then(function (resp2) {
-        console.log(resp2);
-        $(".meal-description").text(resp2.meals.strInstructions);
-        console.log(resp2.meals);
-        for (let i = 1; i < 21; i++) {
-            const element = resp2.meals.strIngredient[i];
+        $(".meal-description").text(resp2.meals[0].strInstructions);
+            const element = resp2.meals[0];
+            //console.log(element.strIngredient+[i]);
+            // for("strIngredient"+i in element){
+            //     console.log();
+            // }
+            var ingredientsArray = [];
+            var measurementsArray = [];
+            for (const property in element) {
+                if (element[property]?.length) {
+                    if (property.includes("strIngredient")) {
+                        console.log(element[property].length);
+                        ingredientsArray.push(element[property]);
+                        // $(".ingredients").html("<li>"+element[property]+"</li>")
+                    }
+                    if (property.includes("strMeasure")) {
+                        console.log(element[property]);
+                        measurementsArray.push(element[property]);
+                        // $(".ingredients li").append(element[property]+"</li>")
+                    }
+                }// console.log(`${property}: ${element[property]}`);
 
+            }
+            for (let i = 0; i < ingredientsArray.length; i++) {
+                console.log(ingredientsArray[i]);
+                console.log(measurementsArray[i]);
+                var ingredientsList = $("<li>").text(`${ingredientsArray[i]}: ${measurementsArray[i]}`)
+                $(".ingredients").append(ingredientsList);
+            }
             //trying to get ingredients
-            $(".ingredients").append("<li>" + element + "</li>");
-            console.log(element);
+            // $(".ingredients").append("<li>" + element + "</li>");
+            // console.log(element);
+            // console.log(element.strIngredient+num);
             
-        }
         
     })
 }
 
 //on click of button finds recipes for things you have some ingredients for
 $(".submit").click(function() {
+    // Finds Meals based on ingredients in fridge
     ingredients = $(".ingredients").val();
+    var mealDbURL = `${mealDBPrefix}/filter.php?i=${ingredients}`;
+    console.log(ingredients);
+    console.log(mealDbURL);
     $.ajax({
         url: mealDbURL,
-        method: "GET"
+        method: "GET",
+        timeout: 1000
       }).then(function (resp) {
-          $(".recipe-choice").html("<p>" + resp.meals[1].strMeal + "</p>");
-          console.log(resp.meals[1].strMeal);
-          let img = $("<img>").attr("src", resp.meals[1].strMealThumb);
-          $(".recipe-choice").append(img);
-          meal = resp.meals[1].strMeal.replace(' ', '%20');
-          console.log(meal);
-          test = `https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`;
-          console.log(test);
-          console.log(resp);
-          setTimeout(testAPI2(resp), 1000);
+          for (let i = 0; i < 4; i++) {
+              console.log(mealDbURL);
+              $(".recipe-choice").html("<p>" + resp.meals[i].strMeal + "</p>");
+              console.log(resp.meals[1].strMeal);
+              let img = $("<img>").attr("src", resp.meals[i].strMealThumb);
+              $(".recipe-choice").append(img);
+              meal = resp.meals[i].strMeal.replace(' ', '%20');
+              console.log(meal);
+    
+              //url for a single meal
+              test = `https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`;
+              console.log(resp);
+              testAPI2(resp);
+    
+          }
       });
 })
 
